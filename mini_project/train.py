85e40a901283eb69e74ad0d05c653817005b12b6
AmirMohammadHakimiyan <amirmohammadhakimiyan6@gmail.com>
2023-09-11 03:17:09 -0700

    mini_project

--- mini_project/train.py
+++ mini_project/train.py
@@ -4,21 +4,21 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
 df=pd.read_csv("data.csv")
-df["chahat"]=df["act1"]
+df["direction"]=df["act1"]
 for i in df.index:
     if df.loc[i,"act1"]==-1 and df.loc[i,"act2"]==0:
-        df.loc[i,"chahat"]=0
+        df.loc[i,"direction"]=0
     elif df.loc[i,"act1"]==0 and df.loc[i,"act2"]==-1:
-        df.loc[i,"chahat"]=1
+        df.loc[i,"direction"]=1
     elif df.loc[i,"act1"]==1 and df.loc[i,"act2"]==0:
-        df.loc[i,"chahat"]=2
+        df.loc[i,"direction"]=2
     elif df.loc[i,"act1"]==0 and df.loc[i,"act2"]==1:
-        df.loc[i,"chahat"]=3
+        df.loc[i,"direction"]=3
 
 df.drop("act1",axis=1,inplace=True)
 df.drop("act2",axis=1,inplace=True)
-X=df.drop("chahat",axis=1).to_numpy()
-Y=df["chahat"].to_numpy()
+X=df.drop("direction",axis=1).to_numpy()
+Y=df["direction"].to_numpy()
 X_train,X_test,Y_train,Y_test=train_test_split(X,Y)
 model=tf.keras.models.Sequential([tf.keras.layers.Dense(8,activation="relu"),
                                 tf.keras.layers.Dense(16,activation="relu"),
